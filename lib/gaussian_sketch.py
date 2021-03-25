0d35b24133933c358c2a0ca9c2f7046b95387947
Charlie <dickens.c07@gmail.com>
2021-03-25 11:21:30 +0000

    Added init for gaussian sketch

---
+++ lib/gaussian_sketch.py
@@ -0,0 +1,29 @@
+import numpy as np
+from matrix_sketch import MatrixSketch
+
+class GaussianSketch(MatrixSketch):
+    """
+    Class wrapper for the Gaussian Sketch
+    """
+    def __init__(self,sketch_dim:int, n_data_rows:int, \
+        n_data_cols:int):
+
+        super(GaussianSketch,self).__init__(sketch_dim)
+        super(GaussianSketch,self)._prepare_sketch(n_data_rows, n_data_cols)
+        self._sample()
+        print('Init Gausss')
+
+    def _sample(self,seed=100):
+        """
+        Samples the random linear transform S with 'seed'
+        S is a Gaussian random transform.
+        """
+        np.random.seed(seed)
+        S = np.random.randn(self.sketch_dim, self.n_data_rows)
+        S /= np.sqrt(self.sketch_dim)
+        return S
+
+    def sketch_matrix(self,mat,seed=None):
+        S = self._sample(seed)
+        self.sketch_matrix = S@mat
+        
---
+++ lib/matrix_sketch.py
@@ -0,0 +1,35 @@
+import numpy as np 
+
+class MatrixSketch:
+    """
+    A generic class which calls different matrix sketches.
+
+    Applies a sketch to matrix A of size n \times d to generate 
+    a sketch B of size m \times d.
+    """
+
+    def __init__(self,sketch_dim:int):
+        """
+        Inputs: 
+            sketch_dim - the number of rows to project down to for B
+        """
+        self.sketch_dim = sketch_dim
+        print('Init MatrixSketch')
+        print('Sketch Dim:', self.sketch_dim)
+
+    def _prepare_sketch(self,n_data_rows:int, n_data_cols:int):
+        """
+        Helper function to set parameters of the sketch and data we may need
+        """
+        self.n_data_rows = n_data_rows
+        self.n_data_cols = n_data_cols
+
+    def get(self,in_svd=False):
+        """
+        Returns the sketch self.sketch_matrix either in full form or in SVD form
+        """
+        if in_svd:
+            u,s,vt = np.linalg.svd(self.sketch_matrix)
+            return u,s,vt
+        else:
+            return self.sketch_matrix
