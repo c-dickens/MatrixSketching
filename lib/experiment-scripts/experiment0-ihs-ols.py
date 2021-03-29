83df751a74668a7652a7836fbe48c261ef99f2a9
Charlie <dickens.c07@gmail.com>
2021-03-29 18:20:09 +0100

    Added classical sketch to exp

--- lib/experiment-scripts/experiment0-ihs-ols.py
+++ lib/experiment-scripts/experiment0-ihs-ols.py
@@ -1,5 +1,9 @@
+import sys
+sys.path.append('../')
 import numpy as np
 import pandas as pd
+from classical_sketch import ClassicalSketch
+
 from experiment_utils import prediction_error
 
 def experimental_data(n,d, sigma=1.0,seed=100):
@@ -28,21 +32,35 @@ def main():
     # * Experimental setup 
     nn  = np.array([100*2**_ for _ in range(5)])
     d = 10
-    num_trials = 2
+    num_trials = 5
 
     # * Results setup 
     results_df = pd.DataFrame()
     results_df['Rows'] = nn
     opt_results = np.zeros_like(nn,dtype=float)
+    classical_results = np.zeros_like(opt_results)
 
     for i,n in enumerate(nn):
         for t in range(num_trials):
             y, A, x_model = experimental_data(n,d,seed=t)
+
+            # * Optimal weights
             x_opt = np.linalg.lstsq(A,y)[0]
             error = prediction_error(A,x_model,x_opt)
             opt_results[i] += error
+
+            # * Sketched weights
+            # Sketch parameters taken from IHS paper
+            classical_sk_dim = 1 + np.int(np.ceil(np.log(n)))
+            classical_sk_dim *= 5*d
+            classical_sk_solver = ClassicalSketch(n,d,classical_sk_dim,'SRHT')
+            x_sk = classical_sk_solver.fit(A,y,seed=t)
+            classical_error = prediction_error(A,x_model,x_sk)
+            classical_results[i] += classical_error
         opt_results[i] /= num_trials
+        classical_results[i] /= num_trials
     results_df['Optimal'] = opt_results
+    results_df['Classical'] = classical_results
     print(results_df)
     print(opt_results)
 if __name__ == '__main__':
