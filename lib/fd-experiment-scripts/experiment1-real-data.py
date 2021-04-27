dca96c1ca150ff817a6a26bb9503de29387fe99d
Charlie <dickens.c07@gmail.com>
2021-04-27 15:51:06 +0100

    Converted old ifdrr code to current framework

--- lib/fd-experiment-scripts/experiment1-real-data.py
+++ lib/fd-experiment-scripts/experiment1-real-data.py
@@ -19,6 +19,7 @@ from timeit import default_timer as timer
 import pprint
 from iterative_ridge_regression import IterativeRidge
 from experiment_utils import svd_ridge_solve, get_euclidean_errors
+from scipy.sparse import coo_matrix
 
 def real_data_error_profile(data_name,sketch_size):
     '''
@@ -26,7 +27,7 @@ def real_data_error_profile(data_name,sketch_size):
     features
     '''
     # * Experimental parameters
-    n = 20000
+    n = 10000
     trials = 1
     iterations = 10
     # ds = DataFactory(n=n)
@@ -57,26 +58,25 @@ def real_data_error_profile(data_name,sketch_size):
         'iteration times' : { _ : np.zeros(trials,dtype=float) for _ in features},
         'all_times'       : { _ : np.zeros((iterations+1, trials),dtype=float) for _ in features}
     }
-    rpg_results = { 
+    rp_srht_results = { 
         'errors'          : { _ : np.zeros((iterations+1, trials),dtype=float) for _ in features},
         'build times'     : { _ : np.zeros(trials,dtype=float) for _ in features},
         'iteration times' : { _ : np.zeros(trials,dtype=float) for _ in features},
         'all_times'       : { _ : np.zeros((iterations+1, trials),dtype=float) for _ in features}
     }
-    rps_results = { 
+    rp_cntsk_results = { 
         'errors'          : { _ : np.zeros((iterations+1, trials),dtype=float) for _ in features},
         'build times'     : { _ : np.zeros(trials,dtype=float) for _ in features},
         'iteration times' : { _ : np.zeros(trials,dtype=float) for _ in features},
         'all_times'       : { _ : np.zeros((iterations+1, trials),dtype=float) for _ in features}
     }
-    ihs_g_results=  { 
+    ihs_srht_results =  { 
         'errors'          : { _ : np.zeros((iterations+1, trials),dtype=float) for _ in features},
         'build times'     : { _ : np.zeros(trials,dtype=float) for _ in features},
         'iteration times' : { _ : np.zeros(trials,dtype=float) for _ in features},
         'all_times'       : { _ : np.zeros((iterations+1, trials),dtype=float) for _ in features}
     }
-
-    ihs_s_results =  { 
+    ihs_countsketch_results =  { 
         'errors'          : { _ : np.zeros((iterations+1, trials),dtype=float) for _ in features},
         'build times'     : { _ : np.zeros(trials,dtype=float) for _ in features},
         'iteration times' : { _ : np.zeros(trials,dtype=float) for _ in features},
@@ -105,9 +105,10 @@ def real_data_error_profile(data_name,sketch_size):
                 X_poly = RBFSampler(gamma=0.0001, random_state=t,n_components=feature_hyper).fit_transform(X_sample)
             X = StandardScaler().fit_transform(X_poly)
             N,D = X.shape
+            X_train_sparse = coo_matrix(X) 
             g = np.linalg.norm(X,ord='fro')**2 / sketch_size 
             print('#'*10, f'\t GAMMA: G={g}, i={i} d={feature_hyper}\t', '#'*10)
-            print('MAtrix norm: ', np.linalg.norm(X,ord='fro')**2 / sketch_size)
+
             # # ! Optimal solution
             print('#'*60)
             print('Solving exactly: Data shape: ', X.shape)
@@ -117,11 +118,11 @@ def real_data_error_profile(data_name,sketch_size):
             exact_results['solve time'][feature_hyper][t] = solve_time
 
 
-            # # ! FD Sketching 
+            # ! FD Sketching 
             print('#'*10, '\t FREQUENT DIRECTIONS \t', '#'*10)
             #fdr = FDRidge(fd_dim=sketch_size,gamma=g)
             fdr = IterativeRidge(N,D,sk_dim=sketch_size,sk_mode='FD',gamma=g)
-            _, all_x,fd_measured = fdr.fast_iterate(X,y,iterations)
+            _, all_x,fd_measured = fdr.fit(X,y,iterations)
 
             fd_results['errors'][feature_hyper][:,t]             =  get_euclidean_errors(all_x,x_opt)
             fd_results['build times'][feature_hyper][t]          =  fd_measured['sketch time']
@@ -133,87 +134,97 @@ def real_data_error_profile(data_name,sketch_size):
             print('#'*10, '\t ROBUST FREQUENT DIRECTIONS \t', '#'*10)
             #rfdr = FDRidge(fd_dim=sketch_size,fd_mode='RFD',gamma=g)
             rfdr = IterativeRidge(N,D,sk_dim=sketch_size,sk_mode='RFD',gamma=g)
-            _, rfd_all_x,rfd_measured = rfdr.fast_iterate(X,y,iterations)
+            _, rfd_all_x,rfd_measured = rfdr.fit(X,y,iterations)
             
             rfd_results['errors'][feature_hyper][:,t]             =  get_euclidean_errors(rfd_all_x,x_opt)
             rfd_results['build times'][feature_hyper][t]          =  rfd_measured['sketch time']
             rfd_results['iteration times'][feature_hyper][t]      =  mean_iter_time_single(rfd_measured)
             rfd_results['all_times'][feature_hyper][:,t]          =  rfd_measured['all_times']
 
-            # # # ! Single Random sketches
-            # # print('#'*10, '\t GAUSS SINGLE \t', '#'*10)
-            # gauss_single = RPRidge(rp_dim=sketch_size,rp_mode='Gaussian',gamma=g)
-            # _, gauss_single_all_x, gauss_single_measured = gauss_single.iterate_single_timing(X,y,seed=i)
+            # # ! Single Random sketches
+            print('#'*10, '\t SRHT SINGLE \t', '#'*10)
+            srht_single = IterativeRidge(N,D,sk_dim=sketch_size,sk_mode='SRHT',gamma=g) # RPRidge(rp_dim=sketch_size,rp_mode='Gaussian',gamma=g)
+            _, srht_single_all_x, srht_single_measured = srht_single.fit(X,y,seed=i)
 
-            # rpg_results['errors'][feature_hyper][:,t]             =  get_errors(gauss_single_all_x,x_opt)
-            # rpg_results['build times'][feature_hyper][t]          =  gauss_single_measured['sketch time']
-            # rpg_results['iteration times'][feature_hyper][t]      =  mean_iter_time_single(gauss_single_measured)
-            # rpg_results['all_times'][feature_hyper][:,t]          =  gauss_single_measured['all_times']
+            rp_srht_results['errors'][feature_hyper][:,t]             =  get_euclidean_errors(srht_single_all_x,x_opt)
+            rp_srht_results['build times'][feature_hyper][t]          =  srht_single_measured['sketch time']
+            rp_srht_results['iteration times'][feature_hyper][t]      =  mean_iter_time_single(srht_single_measured)
+            rp_srht_results['all_times'][feature_hyper][:,t]          =  srht_single_measured['all_times']
 
-            # print('#'*10, '\t SJLT SINGLE \t', '#'*10)
-            # sjlt_single = RPRidge(rp_dim=sketch_size,rp_mode='SJLT',gamma=g)
-            # _, sjlt_single_all_x, sjlt_single_measured = sjlt_single.iterate_single_timing(X,y)
+            print('#'*10, '\t CountSketch SINGLE \t', '#'*10)
+            cntsk_single = IterativeRidge(N,D,sk_dim=sketch_size,sk_mode='CountSketch',gamma=g,sparse_data=X_train_sparse) #RPRidge(rp_dim=sketch_size,rp_mode='SJLT',gamma=g)
+            _, cntsk_single_all_x, cntsk_single_measured = cntsk_single.fit(X,y)
 
-            # rps_results['errors'][feature_hyper][:,t]             =  get_errors(sjlt_single_all_x,x_opt)
-            # rps_results['build times'][feature_hyper][t]          =  sjlt_single_measured['sketch time']
-            # rps_results['iteration times'][feature_hyper][t]      =  mean_iter_time_single(sjlt_single_measured)
-            # rps_results['all_times'][feature_hyper][:,t]          =  sjlt_single_measured['all_times']
+            rp_cntsk_results['errors'][feature_hyper][:,t]             =  get_euclidean_errors(cntsk_single_all_x,x_opt)
+            rp_cntsk_results['build times'][feature_hyper][t]          =  cntsk_single_measured['sketch time']
+            rp_cntsk_results['iteration times'][feature_hyper][t]      =  mean_iter_time_single(cntsk_single_measured)
+            rp_cntsk_results['all_times'][feature_hyper][:,t]          =  cntsk_single_measured['all_times']
 
-            # print('#'*10, '\t SJLT IHS \t', '#'*10)
-            # ihs_sjlt = RPRidge(rp_dim=sketch_size,rp_mode='SJLT',gamma=g)
-            # _, ihs_sjlt_all_x, ihs_sjlt_measured = ihs_sjlt.iterate_multiple_timing(X,y)
-             
-            # ihs_s_results['errors'][feature_hyper][:,t]              =  get_errors(ihs_sjlt_all_x,x_opt)
-            # ihs_s_results['build times'][feature_hyper][t]           =  ihs_sjlt_measured['sketch time']
-            # ihs_s_results['iteration times'][feature_hyper][t]       =  mean_iter_time_multi(ihs_sjlt_measured)
-            # ihs_s_results['all_times'][feature_hyper][:,t]           =  ihs_sjlt_measured['all_times']
 
-            # # ! Multi Random sketches
-            # print('#'*10, '\t GAUSS IHS \t', '#'*10)
-            # ihs_gauss = RPRidge(rp_dim=sketch_size,rp_mode='Gaussian',gamma=g)
-            # _, ihs_gauss_all_x, ihs_gauss_measured = ihs_gauss.iterate_multiple_timing(X,y)
+            # ! Multi Random sketches           
+            print('#'*10, '\t CountSketch IHS \t', '#'*10)
+            ihs_cntsk = IterativeRidge(N,D,sk_dim=sketch_size,sk_mode='CountSketch',gamma=g,sparse_data=X_train_sparse,ihs_mode='multi') #RPRidge(rp_dim=sketch_size,rp_mode='SJLT',gamma=g)
+            _, ihs_cntsk_all_x, ihs_cntsk_measured = ihs_cntsk.fit(X,y)
+            
+              
+            ihs_countsketch_results['errors'][feature_hyper][:,t]              =  get_euclidean_errors(ihs_cntsk_all_x,x_opt)
+            ihs_countsketch_results['build times'][feature_hyper][t]           =  np.sum(ihs_cntsk_measured['sketch time'])
+            ihs_countsketch_results['iteration times'][feature_hyper][t]       =  mean_iter_time_multi(ihs_cntsk_measured)
+            ihs_countsketch_results['all_times'][feature_hyper][:,t]           =  ihs_cntsk_measured['all_times']
 
-            # ihs_g_results['errors'][feature_hyper][:,t]              =  get_errors(ihs_gauss_all_x,x_opt)
-            # ihs_g_results['build times'][feature_hyper][t]           =  ihs_gauss_measured['sketch time']
-            # ihs_g_results['iteration times'][feature_hyper][t]       =  mean_iter_time_multi(ihs_gauss_measured)
-            # ihs_g_results['all_times'][feature_hyper][:,t]           =  ihs_gauss_measured['all_times']
+
+            print('#'*10, '\t SRHT IHS \t', '#'*10)
+            ihs_srht = IterativeRidge(N,D,sk_dim=sketch_size,sk_mode='SRHT',gamma=g,ihs_mode='multi')#  RPRidge(rp_dim=sketch_size,rp_mode='Gaussian',gamma=g)
+            _, ihs_srht_all_x, ihs_srht_measured = ihs_srht.fit(X,y)
+
+            ihs_srht_results['errors'][feature_hyper][:,t]              =  get_euclidean_errors(ihs_srht_all_x,x_opt)
+            ihs_srht_results['build times'][feature_hyper][t]           =  np.sum(ihs_srht_measured['sketch time'])
+            ihs_srht_results['iteration times'][feature_hyper][t]       =  mean_iter_time_multi(ihs_srht_measured)
+            ihs_srht_results['all_times'][feature_hyper][:,t]           =  ihs_srht_measured['all_times']
 
 
     # ! Prepare and save the results in json format 
     pp = pprint.PrettyPrinter(indent=4)
-    print('Gauss')
-    pp.pprint(fd_results)
+    # print('FD')
+    # pp.pprint(fd_results['errors'])
+    # print('SRHT-Single')
+    # pp.pprint(rp_srht_results['errors'])
+    # print('SRHT-Multi')
+    # pp.pprint(ihs_g_results['errors'])
     # # print('Gauss')
     # # pp.pprint(rpg_results)
-    # # print('ihs:Gauss')
-    # # pp.pprint(ihs_g_results)
-    # # # print('SJLT')
-    # # # pp.pprint(rps_results)
-    # # print('ihs:SJLT')
-    # # pp.pprint(ihs_s_results)
+    # print('ihs:SRHT')
+    # pp.pprint(ihs_srht_results)
+    # print('SJLT')
+    # pp.pprint(rp_cntsk_results)
+    # print('ihs:SJLT')
+    # pp.pprint(ihs_countsketch_results)
 
 
 
 
-    # results_file_name = 'results/experiment3-real-data/' + data_name + '.json'
-    # for d in [exact_results, fd_results, rfd_results, rpg_results, rps_results, ihs_g_results, ihs_s_results]:
-    #     for k,v in d.items():
-    #         for v_key, v_val in v.items():
-    #             if type(v_val) == np.ndarray:
-    #                 d[k][v_key] = v_val.tolist()
-    # all_results = {
-    #     'Exact'     : exact_results,
-    #     'FD'        : fd_results,
-    #     'RFD'       : rfd_results,
-    #     'Gauss'     : rpg_results,
-    #     'SJLT'      : rps_results,
-    #     'ihs:Gauss' : ihs_g_results,
-    #     'ihs:SJLT'  : ihs_s_results
-    # }
+    
+    
+    
+    results_file_name = 'results/real_data/error_profile' + data_name + '.json'
+    for d in [exact_results, fd_results, rfd_results, rp_srht_results, rp_cntsk_results, ihs_srht_results, ihs_countsketch_results]:
+        for k,v in d.items():
+            for v_key, v_val in v.items():
+                if type(v_val) == np.ndarray:
+                    d[k][v_key] = v_val.tolist()
+    all_results = {
+        'Exact'     : exact_results,
+        'FD'        : fd_results,
+        'RFD'       : rfd_results,
+        'SRHT'      : rp_srht_results,
+        'CountSketch'      : rp_cntsk_results,
+        'ihs:SRHT' : ihs_srht_results,
+        'ihs:CountSketch'  : ihs_countsketch_results
+    }
 
 
-    # with open(results_file_name, 'w') as fp:
-    #     json.dump(all_results, fp,sort_keys=True, indent=4)  
+    with open(results_file_name, 'w') as fp:
+        json.dump(all_results, fp,sort_keys=True, indent=4)  
 
 
 def main():
--- lib/iterative_ridge_regression.py
+++ lib/iterative_ridge_regression.py
@@ -2,6 +2,10 @@ import numpy as np
 import sys
 import os
 from timeit import default_timer as timer
+from gaussian_sketch import GaussianSketch
+from count_sketch import CountSketch
+from sparse_jlt import SparseJLT
+from srht_sketch import SRHTSketch
 # from pathlib import Path
 # path = Path(os.getcwd())
 # sys.path.append(str(path.parent.parent/ 'src'))
@@ -30,8 +34,9 @@ class IterativeRidge:
         self.n_data_rows  = n_data_rows
         self.n_data_cols  = n_data_cols
         self.sk_dim       = min([sk_dim,n_data_cols]) #sk_dim
-        if self.sk_mode not in ['FD', 'RFD']:
-            raise NotImplementedError('Only F(ast) and R(obust) FD methods are supported.')
+        self.ihs_mode     = ihs_mode
+        # if self.sk_mode not in ['FD', 'RFD']:
+        #     raise NotImplementedError('Only F(ast) and R(obust) FD methods are supported.')
        
         if self.sk_mode == 'Gaussian':
             self.sketcher = GaussianSketch(self.sk_dim,self.n_data_rows,self.n_data_cols)
@@ -44,9 +49,12 @@ class IterativeRidge:
             self.sketcher = SparseJLT(self.sk_dim,self.n_data_rows,self.n_data_cols,col_sparsity=sjlt_sparsity)
         elif self.sk_mode == 'CountSketch':
             self.sketcher = CountSketch(self.sk_dim,self.n_data_rows,self.n_data_cols)
+        # Set ihs mode to be single for deterministic sketches
         elif self.sk_mode == 'FD':
+            self.ihs_mode = 'single'
             self.sketcher = FastFrequentDirections(self.sk_dim,self.n_data_rows,self.n_data_cols)
         elif self.sk_mode == 'RFD':
+            self.ihs_mode = 'single'
             self.sketcher = RobustFrequentDirections(self.sk_dim,self.n_data_rows,self.n_data_cols)
 
         if batch_size == None:
@@ -54,71 +62,13 @@ class IterativeRidge:
         else:
             self.batch_size = batch_size
 
-    def _init_iterations(self,X,y,iterations):
-        """
-        Initialises the arrays we use for iterations
-        - current_weights the vector we will update under iterative scheme
-        - weights_hist is an array which contains all of the updated weights used for error history
-        - XTy is the projection of the targets onto the column space of the data X
-        """
-        current_weights = np.zeros((self.n_data_cols,1),dtype=float)
-        weights_hist = np.zeros((self.n_data_cols,iterations),dtype=float)
-        XTy = (X.T@y).reshape(-1,1)
-        return current_weights, weights_hist, XTy
+        self.sparse_data_is_set = False # Init for dense sketches to False
+        if (sparse_data is not None) and (self.sk_mode == 'SJLT' or self.sk_mode == 'CountSketch'):
+            self.sketcher.set_sparse_data(sparse_data)
+            self.sparse_data_is_set = True
 
-    def _grad(self, X, vec, XTy):
-        """
-        Returns the gradient function 
-        nabla f(x) = (X.T X + gamma I)w - XTy
-        We use as input the vector XTy so no need to recompute
-        """
-        return X.T@(X@vec) + self.gamma*vec  - XTy
 
-    # def _sketch(self, X):
-    #     '''
-    #     Private function for calling the sketch methods
-    #     '''
-    #     if self.sk_mode == 'FD':
-    #         sketcher = FastFrequentDirections(X.shape[1],sketch_dim=self.sk_dim)
-    #     elif self.sk_mode == 'RFD':
-    #         sketcher = RobustFrequentDirections(X.shape[1],sketch_dim=self.sk_dim)
-    #     sketcher.fit(X,batch_size=self.batch_size)
-    #     self.sketch = sketcher.sketch
-    #     self.alpha = sketcher.delta # == 0 if using FastFrequentDirections so can use self.gamma + self.alpha everywhere 
-    #     self.is_fitted = True
-    
-    # def fit(self,X,y):
-    #     '''
-    #     Fits the ridge regression model on data X with targets y
-    #     '''
-    #     d = X.shape[1]
-    #     self._sketch(X)
-    #     H = self.sketch.T@self.sketch + (self.gamma+self.alpha)*np.eye(d)
-    #     self.coef_ = np.linalg.solve(H, X.T@y)
-    #     self.H_inv = np.linalg.pinv(H)
-
-    def iterate(self,X,y,iterations=10,timing=False,seed=1000):
-        current_x, all_x, XTy = self._init_iterations(X,y,iterations)
-        if self.sk_mode in ['FD','RFD']:
-            print('Sketching data')
-            self.sketcher.sketch(X, batch_size=self.batch_size)
-            _, sigma_squared, vt, delta = self.sketcher.get_fd_outputs()
-            sig = np.sqrt(sigma_squared)
-        else:
-            self.sketcher.sketch(X,seed=seed*100)
-            _,sig,vt = self.sketcher.get(in_svd=True)
-        sig = sig[:,np.newaxis]
-        sig_inv = 1./sig
-        for it in range(iterations):
-            #######################################################
-            # 1. Generate a sketch and obtain the svd factors for efficient solving.
-            gradient = self._grad(X, current_x, XTy)
-            update = - vt.T@ (sig_inv**2 * (vt @ gradient)) # This solves lineat system H update = - gradient
-            current_x += update
-            all_x[:,it] = current_x[:,0]
-        return current_x, all_x
-        
-    def fast_iterate(self,X,y,iterations,seed=100):
+    def fast_iterate_single(self,X,y,iterations,seed=100):
         """
         Performs the iterations of ifdrr efficiently in small space and time.
         """
@@ -127,15 +77,11 @@ class IterativeRidge:
         d = X.shape[1]
         w = np.zeros((d,1),dtype=float)
         all_w = np.zeros((d,iterations))
-        # if self.sk_mode == 'FD':
-        #     sketcher = FastFrequentDirections(self.sk_dim,X.shape[0], X.shape[1]) # FastFrequentDirections(X.shape[1],sketch_dim=self.sk_dim)
-        # elif self.sk_mode == 'RFD':
-        #     sketcher = RobustFrequentDirections(self.sk_dim,X.shape[0], X.shape[1])  # (X.shape[1],sketch_dim=self.sk_dim)
         measurables = {
         'sketch time' : None,
+        'update time' : np.zeros(iterations,dtype=float),
         'all_times'   : np.zeros(iterations+1,dtype=float),
-        'gradients'   : np.zeros((d,iterations),dtype=float),
-        'updates'     : np.zeros((d,iterations),dtype=float),
+        'total'       : None
         # 'sketch'      : None
         }
 
@@ -149,30 +95,99 @@ class IterativeRidge:
         else:
             TIMER_START = timer()
             self.sketcher.sketch(X,seed=109*seed)
-            _, sig, vt = self.sketcher.get(in_svd=True)
+            _, sig, Vt = self.sketcher.get(in_svd=True)
             SigSq = sig**2
+            implicit_reg = 0.0 # the randomised methods have no implicit reg parameters
         SKETCH_TIME = timer() - TIMER_START
-        V = Vt.T
-        invTerm = (1./(SigSq + implicit_reg + self.gamma )).reshape(-1,1)
+
 
         # Extra parameters we may need 
         XTy = (X.T@y).reshape(-1,1)
+        V = Vt.T
+        invTerm = (1./(SigSq + implicit_reg + self.gamma )).reshape(-1,1)
 
         # * This lambda function evaluates H^{-1}g efficiently for gradient vector g
         H_inv_grad = lambda g, vtg : (1/self.gamma )*(g - V@vtg) + V@(invTerm*vtg)
         ridge_grad = lambda w : X.T@(X@w) + self.gamma*w - XTy
 
-        for it in range(iterations):   
+        for it in range(iterations):  
+            UPDATE_TIMER_START = timer() 
             grad = ridge_grad(w) #X.T@(X@w) + self.gamma*w - XTy
             VTg = Vt@grad
             update = H_inv_grad(grad, VTg)
             w += - update
-            all_w[:,it] = np.squeeze(w)
+            measurables['update time'][it] = timer() - UPDATE_TIMER_START
             measurables['all_times'][it+1] = timer() - TIMER_START
-            measurables['gradients'][:,it] = np.squeeze(grad)
-            measurables['updates'][:,it] = np.squeeze(update)
+            all_w[:,it] = np.squeeze(w)
         measurables['sketch time'] = SKETCH_TIME
+        measurables['total'] = timer() - TIMER_START
         return w, all_w, measurables
+
+    def _init_iterations_multi(self,X,y,iterations):
+        """
+        Initialises the arrays we use for iterations
+        - current_weights the vector we will update under iterative scheme
+        - weights_hist is an array which contains all of the updated weights used for error history
+        - XTy is the projection of the targets onto the column space of the data X
+        """
+        current_weights = np.zeros((self.n_data_cols,1),dtype=float)
+        weights_hist = np.zeros((self.n_data_cols,iterations),dtype=float)
+        measurables = {
+        'sketch time' : np.zeros(iterations,dtype=float),
+        'update time' : np.zeros(iterations,dtype=float),
+        'all_times'   : np.zeros(iterations+1,dtype=float),
+        'SVD'         : np.zeros(iterations,dtype=float),
+        'Solve'       : np.zeros(iterations,dtype=float),
+        }
+        XTy = (X.T@y).reshape(-1,1)
+        return current_weights, weights_hist,measurables, XTy
+
+    def fast_iterate_multi(self,X,y,iterations,seed=100):
+        """
+        Performs the iterations of ifdrr efficiently in small space and time.
+        """
+
+        # * Initialisation not timed
+        w,all_w,measurables,XTy = self._init_iterations_multi(X,y,iterations)
+ 
+
+        # ! Sketching
+        if self.sk_mode in ['FD', 'RFD']:
+            raise NotImplementedError('F(ast) and R(obust) FD methods are supported only for ihs_mode = single.')
+
+        # * This lambda function evaluates H^{-1}g efficiently for gradient vector g
+        H_inv_grad = lambda g, vtg, V : (1/self.gamma )*(g - V@vtg) + V@(invTerm*vtg)
+        ridge_grad = lambda w : X.T@(X@w) + self.gamma*w - XTy
+
+        TIMER_START = timer()
+        for it in range(iterations):   
+            SKETCH_TIMER = timer()
+            self.sketcher.sketch(X,seed=1000*it)
+            measurables['sketch time'][it] = timer() - SKETCH_TIMER
+
+            SVD_TIMER = timer()
+            u,sig,Vt = self.sketcher.get(in_svd=True)
+            sig = sig[:,np.newaxis]
+            SigSq = sig**2
+            invTerm = (1./(SigSq + self.gamma )).reshape(-1,1)
+
+            grad = ridge_grad(w)
+            VTg = Vt@grad
+            update = H_inv_grad(grad, VTg, Vt.T)
+            w += - update
+            measurables['update time'][it] = timer() - TIMER_START
+            measurables['all_times'][it+1] = timer() - TIMER_START
+            all_w[:,it] = np.squeeze(w)
+        measurables['total']= timer() - TIMER_START
+        return w, all_w, measurables
+
+    def fit(self,X,y,iterations=10,timing=False,seed=9999):
+        if self.ihs_mode == 'single':
+            return self.fast_iterate_single(X,y,iterations,seed)
+        else:
+            return self.fast_iterate_multi(X,y,iterations)
+
+            
 
 
 # import numpy as np 
@@ -277,3 +292,55 @@ class IterativeRidge:
 #         return x, all_x
 
 
+
+    # def _grad(self, X, vec, XTy):
+    #     """
+    #     Returns the gradient function 
+    #     nabla f(x) = (X.T X + gamma I)w - XTy
+    #     We use as input the vector XTy so no need to recompute
+    #     """
+    #     return X.T@(X@vec) + self.gamma*vec  - XTy
+
+    # def _sketch(self, X):
+    #     '''
+    #     Private function for calling the sketch methods
+    #     '''
+    #     if self.sk_mode == 'FD':
+    #         sketcher = FastFrequentDirections(X.shape[1],sketch_dim=self.sk_dim)
+    #     elif self.sk_mode == 'RFD':
+    #         sketcher = RobustFrequentDirections(X.shape[1],sketch_dim=self.sk_dim)
+    #     sketcher.fit(X,batch_size=self.batch_size)
+    #     self.sketch = sketcher.sketch
+    #     self.alpha = sketcher.delta # == 0 if using FastFrequentDirections so can use self.gamma + self.alpha everywhere 
+    #     self.is_fitted = True
+    
+    # def fit(self,X,y):
+    #     '''
+    #     Fits the ridge regression model on data X with targets y
+    #     '''
+    #     d = X.shape[1]
+    #     self._sketch(X)
+    #     H = self.sketch.T@self.sketch + (self.gamma+self.alpha)*np.eye(d)
+    #     self.coef_ = np.linalg.solve(H, X.T@y)
+    #     self.H_inv = np.linalg.pinv(H)
+
+    # def iterate(self,X,y,iterations=10,timing=False,seed=1000):
+    #     current_x, all_x, XTy = self._init_iterations(X,y,iterations)
+    #     if self.sk_mode in ['FD','RFD']:
+    #         print('Sketching data')
+    #         self.sketcher.sketch(X, batch_size=self.batch_size)
+    #         _, sigma_squared, vt, delta = self.sketcher.get_fd_outputs()
+    #         sig = np.sqrt(sigma_squared)
+    #     else:
+    #         self.sketcher.sketch(X,seed=seed*100)
+    #         _,sig,vt = self.sketcher.get(in_svd=True)
+    #     sig = sig[:,np.newaxis]
+    #     sig_inv = 1./sig
+    #     for it in range(iterations):
+    #         #######################################################
+    #         # 1. Generate a sketch and obtain the svd factors for efficient solving.
+    #         gradient = self._grad(X, current_x, XTy)
+    #         update = - vt.T@ (sig_inv**2 * (vt @ gradient)) # This solves lineat system H update = - gradient
+    #         current_x += update
+    #         all_x[:,it] = current_x[:,0]
+    #     return current_x, all_x
